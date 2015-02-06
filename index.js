b3a9d5ded96f11e34b75edd4a0310fd3be76d849
Avenger7x <avenger7x13@gmail.com>
2015-02-06 13:40:22 +0200

    render function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-module.exports = function(fn){
-	return function(){
-		fn(this.props, this.state);
-	};
+var fnArgs = require('fn-args');
+
+exports.render = function(fn){
+	var args = fnArgs(fn).map(function(name){ return 'this.' + name; });
+
+	return new Function('fn', 'return function(a, b, c){ fn(' + args + ', a, b, c); }')(fn)
 };
