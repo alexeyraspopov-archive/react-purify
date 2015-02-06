f6cb9239ff2a24d3ebfa4317e325d0e0c9c017e2
Avenger7x <avenger7x13@gmail.com>
2015-02-06 13:52:12 +0200

    revert

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ function argsInContext(fn){
 	return fnArgs(fn).map(function(name){ return 'this.' + name; });
 }
 
-exports.render = function(fn){
+module.exports = function(fn){
 	return new Function('fn', 'return function(a, b, c){ fn(' + argsInContext(fn) + ', a, b, c); }')(fn);
-};
-
-exports.state = function(fn){
-	return new Function('fn', 'return function(a, b, c){ var state = fn(' + argsInContext(fn) + ', a, b, c); state && this.setState(state) }')(fn);
 };
